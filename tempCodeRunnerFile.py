de56b30733177c77fcbd31b20e2f58d99349fea8
Harsh <150775589+Harshyadav15@users.noreply.github.com>
2026-02-09 12:46:03 +0530

    Add files via upload

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,18 @@
+import psycopg2
+conn=psycopg2.connect(
+    host="localhost",
+    database="school",
+    user="postgres",
+    password="nikki"
+)
+print("Connection done")
+
+cur=conn.cursor()
+cur.execute("""CREATE TABLE IF NOT EXISTS employee(id INT,name VARCHAR(50),salary INT)""")
+cur.execute("""INSERT INTO employee VALUES(1,'HARSH',5000)""")
+conn.commit()
+cur.execute("""SELECT * from employee""")
+rows=cur.fetchall()
+for row in rows:
+    print(row)
+conn.close()
